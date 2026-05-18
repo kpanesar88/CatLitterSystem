2a648192daf7e7d9aad84f843edac27897d2048a
Karanveer Panesar <Karanveerpanesar04@gmail.com>
2026-05-18 14:39:22 -0400

    uploading photos

--- CatLitterSystem/python/server.py
+++ CatLitterSystem/python/server.py
@@ -1,8 +1,17 @@
+# SERVER IMPORTS
 from flask import Flask
 from flask import request
 
+# FILE IMPORTS
+from datetime import datetime
+from pathlib import Path
+
 app = Flask(__name__)
 
+# CHECK FILE SAVING DIRECTORY
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True);
+    
 # START SERVER ROUTE AT /
 @app.route("/")
 def home():
@@ -10,8 +19,23 @@ def home():
 
 # START POST ROUTE
 @app.route("/upload", methods=["POST"])
-
 def upload():
-    return "upload received"
+    data = request.data
+    
+    if len(data) == 0:
+        return "No Data Received", 400
+    
+    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.jpg")
+    path = UPLOAD_DIR / filename
+    
+    with open(path, "wb") as file:
+        file.write(data)
+        
+    print (f"Saved {path}")
+    
+    return f"Saved {filename}"
+    
+    # print("Received Bytes: ", len(data))
+    # return f"Received{len(data)} bytes"
 
 app.run(host="0.0.0.0", port=5000)
